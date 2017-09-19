eae61d9b0eff8dcbf4570be176f701b633047723
Patrick Kiely <kiely.patrick@gmail.com>
2017-09-19 12:07:25 +0100

    first commit

---
+++ app/controllers/donations.js
@@ -0,0 +1,25 @@
+'use strict';
+
+exports.home = {
+
+  handler: (request, reply) => {
+  reply.file('./app/views/main.html');
+},
+
+};
+
+exports.signup = {
+
+  handler: (request, reply) => {
+    reply.file('./app/views/signup.html');
+  },
+
+};
+
+exports.login = {
+
+  handler: (request, reply) => {
+    reply.file('./app/views/login.html');
+  },
+
+};
---
+++ routes.js
@@ -0,0 +1,18 @@
+const Donations = require('./app/controllers/donations');
+const Assets = require('./app/controllers/assets');
+
+module.exports = [
+
+  { method: 'GET', path: '/', config: Donations.home },
+
+  {
+    method: 'GET',
+    path: '/{param*}',
+    config: { auth: false },
+    handler: Assets.servePublicDirectory,
+  },
+
+  { method: 'GET', path: '/signup', config: Donations.signup },
+  { method: 'GET', path: '/login', config: Donations.login },
+
+];
